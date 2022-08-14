0546c20e84bd19d0b4fee37aa70be99dfafc000e
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2022-08-13 21:59:41 -0700

    feat: workdir

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -3,7 +3,7 @@ set +x
 sshpass -p $CD_PASS ssh -q $CD_USER@$HOST /bin/bash <<EOF
 sudo -su $PODMAN_USER
 export TARGET_ENV=$TARGET_ENV
-export WORKDIR=$WORKDIR
+export PODMAN_WORKDIR=$PODMAN_WORKDIR
 set +o history
 VAULT_TOKEN=$APP_VAULT_TOKEN podman run --rm \
   --security-opt label=disable \
@@ -11,6 +11,6 @@ VAULT_TOKEN=$APP_VAULT_TOKEN podman run --rm \
   --env-host \
   $PODMAN_REGISTRY/$CONTAINER_IMAGE_CONSUL_TEMPLATE \
   -config "/liquibase/changelog/config.hcl" \
-  -template "/liquibase/changelog/liquibase.properties.tpl:${WORKDIR}/liquibase.properties" \
+  -template "/liquibase/changelog/liquibase.properties.tpl:${PODMAN_WORKDIR}/liquibase.properties" \
   -once
 EOF
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,12 +3,12 @@ set +x
 sshpass -p $CD_PASS ssh -q $CD_USER@$HOST /bin/bash <<EOF
 sudo -su $PODMAN_USER
 export TAG_VERSION=$TAG_VERSION
-export WORKDIR=$WORKDIR
+export PODMAN_WORKDIR=$PODMAN_WORKDIR
 # Run update
 podman run --rm \
   --security-opt label=disable \
   -v /tmp/$TMP_VOLUME:/liquibase/changelog \
-  --workdir $WORKDIR \
+  --workdir $PODMAN_WORKDIR \
   $PODMAN_REGISTRY/$CONTAINER_IMAGE_LIQUBASE \
   --defaultsFile=liquibase.properties update-sql
 # Tag version
