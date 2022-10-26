fd071bc8107dc98aade280220cd3359d425136c6
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-10-26 09:56:37 -0700

    fix: echo without newline

--- scripts/vault_db_token.sh
+++ scripts/vault_db_token.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$(echo $INTENTION_JSON | /sw_ux/bin/jq -r '.actions.database.token')
 
-curl -s -X POST $BROKER_URL/v1/provision/token/self \
+echo -n $(curl -s -X POST $BROKER_URL/v1/provision/token/self \
   -H 'Content-Type: application/json' \
   -H 'X-Vault-Role-Id: '"$DB_ROLE_ID"'' -H 'X-Broker-Token: '"$TOKEN"'' | \
-  /sw_ux/bin/jq -r '.wrap_info.token'
+  /sw_ux/bin/jq -r '.wrap_info.token')
