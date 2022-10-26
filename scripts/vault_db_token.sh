d4c322d9508253adbcd8170238b90bd8ed9e4293
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-10-26 08:33:00 -0700

    fix: leave db token unwrapped

--- scripts/vault_db_token.sh
+++ scripts/vault_db_token.sh
@@ -7,4 +7,4 @@ WRAPPED_VAULT_TOKEN=$(curl -s -X POST $BROKER_URL/v1/provision/token/self \
   -H 'X-Vault-Role-Id: '"$DB_ROLE_ID"'' -H 'X-Broker-Token: '"$TOKEN"'' | \
   /sw_ux/bin/jq -r '.wrap_info.token')
 
-VAULT_ADDR=$VAULT_ADDR /sw_ux/bin/vault unwrap -field=token $WRAPPED_VAULT_TOKEN
+echo $WRAPPED_VAULT_TOKEN
