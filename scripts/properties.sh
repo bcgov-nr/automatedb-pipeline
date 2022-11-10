469a9b06e53d8ec7f63681f368c83020dddd8799
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2022-11-10 09:59:21 -0800

    Abort on errors

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -13,4 +13,16 @@ VAULT_TOKEN=$APP_VAULT_TOKEN podman run --rm \
   -config "/liquibase/changelog/config.hcl" \
   -template "/liquibase/changelog/liquibase.properties.tpl:${PODMAN_WORKDIR}/liquibase.properties" \
   -once
+
+PROPERTIES_RC=\$?
+
+# Two exits are required because we are running as another user
+# Only send email for prod
+if [ \$PROPERTIES_RC -ne 0 ]; then
+  if [ "$TARGET_ENV" = "production" ]; then
+    echo "${BUILD_URL}" | mailx -s "Error during Liquibase properties generation" NRIDS.ApplicationDelivery@gov.bc.ca
+  fi
+  exit \$PROPERTIES_RC
+  exit \$PROPERTIES_RC
+fi
 EOF
--- scripts/update.sh
+++ scripts/update.sh
@@ -11,6 +11,16 @@ podman run --rm \
   --workdir $PODMAN_WORKDIR \
   $PODMAN_REGISTRY/$CONTAINER_IMAGE_LIQUBASE \
   --defaultsFile=liquibase.properties update
+
+UPDATE_RC=\$?
+
+# Two exits are required because we are running as another user
+if [ \$UPDATE_RC -ne 0 ] && [ "$TARGET_ENV" = "production" ]; then
+  echo "${BUILD_URL}" | mailx -s "Error during Liquibase update" NRIDS.ApplicationDelivery@gov.bc.ca
+  exit \$UPDATE_RC
+  exit \$UPDATE_RC
+fi
+
 # Tag version
 podman run --rm \
   --security-opt label=disable \
@@ -18,4 +28,16 @@ podman run --rm \
   --workdir $PODMAN_WORKDIR \
   $PODMAN_REGISTRY/$CONTAINER_IMAGE_LIQUBASE \
   --defaultsFile=liquibase.properties tag $TAG_VERSION
+
+TAG_RC=\$?
+
+# Two exits are required because we are running as another user
+# Only send email for prod
+if [ \$TAG_RC -ne 0 ]; then
+  if [ "$TARGET_ENV" = "production" ]; then
+    echo "${BUILD_URL}" | mailx -s "Error during Liquibase tagging" NRIDS.ApplicationDelivery@gov.bc.ca
+  fi
+  exit \$TAG_RC
+  exit \$TAG_RC
+fi
 EOF
